99275066d32794467ad25aa337d0547737eecbea
Adam Bajguz <adam.bajguz@gmail.com>
2019-03-11 22:20:01 +0100

    Removed old code

--- ImageProcessor/ImageProcessor/MainPage.xaml.cs
+++ ImageProcessor/ImageProcessor/MainPage.xaml.cs
@@ -13,13 +13,9 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace ImageProcessor
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         private double zoom = 1;
